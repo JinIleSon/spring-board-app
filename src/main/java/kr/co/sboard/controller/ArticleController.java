c865bf8f3bd98529d1c09f88933caaf53cffcc79
thswlsdlf0000 <sy7521012@naver.com>
2025-09-26 12:21:07 +0900

    0.0.1-SNAPSHOT

--- src/main/java/kr/co/sboard/controller/ArticleController.java
+++ src/main/java/kr/co/sboard/controller/ArticleController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ArticleController {
 
+
+
     @GetMapping("/article/list")
     public String list(){
         return "article/list";
